972994982f1bb11c24324b6b53c2158a2572b85d
Cristian Eduard CHIRA <cristianchira@gmail.com>
2018-10-23 18:43:19 +0300

    xcC

---
+++ SpringMavenSps/src/main/java/com/biostil/SpringMavenSps/Car.java
@@ -0,0 +1,7 @@
+package com.biostil.SpringMavenSps;
+
+public class Car {
+	public void drive(){
+    System.out.print("Car go now...");
+	}
+}
---
+++ SpringMavenSps/src/main/java/com/biostil/SpringMavenSps/IHotDrink.java
@@ -0,0 +1,5 @@
+package com.biostil.SpringMavenSps;
+
+public interface IHotDrink {
+void prepareHotDrink();
+}
