86c1c393e8fb30a5a02d5cb7a817fca84e39c40e
Cristian Eduard CHIRA <cristianchira@gmail.com>
2018-10-24 23:14:01 +0300

    e

--- SpringMavenSps/src/main/java/com/biostil/SpringMavenSps/App.java
+++ SpringMavenSps/src/main/java/com/biostil/SpringMavenSps/App.java
@@ -3,24 +3,17 @@ package com.biostil.SpringMavenSps;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class App 
-{
+
+public class App{ 
+   static int i = 0;
     public static void main( String[] args )
     {
       ApplicationContext context =new ClassPathXmlApplicationContext("SpringConfig.xml");
-      Restaurant restaurantObj =(Restaurant) context.getBean("restaurantBean");
-      restaurantObj.prepareHotDrink();
-     
-      int counter = 0;
-      start: {
-         for (int i = 0; i <= 10; i++) {
-            for (int j = 0; j <= 10; j++) {
-               if (i == 5)
-                  break start;
-            }
-            counter++;
-         }
-      }
-      System.out.println(counter);
-   }
+      //Restaurant restaurantObj =(Restaurant) context.getBean("restaurantBean");
+      //restaurantObj.prepareHotDrink();
+    
+      
+      
+    }
 }
+
